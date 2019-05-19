60f082340e7d24445715f9402d44add38a5bcabb
ekibet <kibetedgar@gmail.com>
2019-05-19 23:31:04 +0300

    alerts action trial

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,20 +2,30 @@ import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Layout/Header'
 import Dashboard from './leads/Dashboard';
+import Alerts from './Layout/alerts';
+
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from 'react-alert-template-basic';
 import { Provider } from "react-redux";
 import store from '../store'
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center'
+}
 class App extends Component {
     state = {}
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header />
-                    <div className="container">
-                        <Dashboard />
-                    </div>
-                </Fragment>
-
+                <AlertProvider template={AlertTemplate}{...alertOptions}>
+                    <Fragment>
+                        <Header />
+                        <Alerts />
+                        <div className="container">
+                            <Dashboard />
+                        </div>
+                    </Fragment>
+                </AlertProvider>
             </Provider>
 
         );

14c0f9013a339842dfe018c327fd1700cc49b266
ekibet <kibetedgar@gmail.com>
2019-05-27 00:55:32 +0300

    create serializers for registeration,login and get user, create api to handle the mentioned functionalities

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer,LoginSerializer
 
 # Register API
 
@@ -16,4 +16,20 @@ class RegisterAPI(generics.GenericAPIView):
         })
 # Login API
 
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    def post(self,request, *args,**kwargs):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user":UserSerializer(user,context = self.get_serializer_context()).data,"token": AuthToken.objects.create(user)[1]
+        })    
 # Get user aPI
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+    def get_object(self):
+        return self.request.user
